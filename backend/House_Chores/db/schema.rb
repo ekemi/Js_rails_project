29062bd85786a527b20f26dde6cc8634a04dd389
Yapnounkie <edmundopierre@yahoo.com>
2021-01-26 18:14:31 -0800

    Create chore model and table

--- backend/House_Chores/db/schema.rb
+++ backend/House_Chores/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_203724) do
+ActiveRecord::Schema.define(version: 2021_01_27_021147) do
+
+  create_table "chores", force: :cascade do |t|
+    t.string "name"
+    t.string "status"
+    t.integer "house_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["house_id"], name: "index_chores_on_house_id"
+  end
 
   create_table "houses", force: :cascade do |t|
     t.string "name"
@@ -19,4 +28,5 @@ ActiveRecord::Schema.define(version: 2021_01_26_203724) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "chores", "houses"
 end
