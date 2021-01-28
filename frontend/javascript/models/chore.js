c09b865a2dd7ca80485dbd080d132fc3f50a0300
Yapnounkie <edmundopierre@yahoo.com>
2021-01-27 19:49:27 -0800

    add index.js and service

--- frontend/javascript/models/chore.js
+++ frontend/javascript/models/chore.js
@@ -7,6 +7,40 @@ class Chore {
         this.id = id;
         Chore.all.push(this) //we put the class in a array, which is putting an object in  
     }
+
+    static postChore(choreData) {
+
+        let formData ={
+
+            "name":choreData.name.value,
+            "status": choreData.status ="Incomplete",
+            "house_id":choreData.quereySelector('select').value //we need a dot 
+
+        }
+        let configObj = {
+
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'content-type': "application/json"
+
+            },
+            body: JSON.stringify(formData)
+        }
+
+        return (Api.CHORES_URL, configObj)
+        .then(response =>response.json())
+        .then(choreObj =>{
+            //find() method return the first array that match the condition
+            let house = House.all.find(chosenFamily =>choreObj.house_id ==chosenFamily.id)
+            let newObj = new Chore(choreObj.name ,choreObj.status, choreObj.id)
+
+            house.chores.push(newObj)
+            clearChoreDivs()
+
+
+        })
+    }
     
 
 }
---
+++ frontend/service/api.js
@@ -0,0 +1,4 @@
+class Api {
+    static baseURL = 'http://127.0.0.1:3000'
+    static CHORES_URL = '${Api.baseURL}/chores';
+}
